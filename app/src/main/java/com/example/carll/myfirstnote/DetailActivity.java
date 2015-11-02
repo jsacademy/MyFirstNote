8b98b7a735d08ad17c819e56acda0a0837f3e1e6
Lee Dongho <carllro@naver.com>
2015-11-02 09:28:55 +0900

    Detail Activity

--- app/src/main/java/com/example/carll/myfirstnote/DetailActivity.java
+++ app/src/main/java/com/example/carll/myfirstnote/DetailActivity.java
@@ -1,20 +1,38 @@
 package com.example.carll.myfirstnote;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.carll.myfirstnote.data.NoteItem;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private NoteItem noteItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = this.getIntent();
+        noteItem = new NoteItem();
+        noteItem.setKey(intent.getStringExtra("key"));
+        noteItem.setValue(intent.getStringExtra("value"));
+
+        EditText editText = (EditText) findViewById(R.id.noteText);
+        editText.setText(noteItem.getValue());
+
+        // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +44,22 @@ public class DetailActivity extends AppCompatActivity {
         });
     }
 
+    private void saveAndFinish() {
+        EditText editText = (EditText) findViewById(R.id.noteText);
+        String noteText = editText.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra("key", noteItem.getKey());
+        intent.putExtra("value", noteText);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            saveAndFinish();
+        }
+        return false;
+    }
 }
